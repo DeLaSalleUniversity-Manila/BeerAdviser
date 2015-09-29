d7ef1d42720d60b8bcc65faf6f1b6ff4bff9b33f
kevindy <kevinmarlson@gmail.com>
2015-09-29 13:30:20 +0800

    added screenshot

---
+++ app/src/main/java/com/example/kevinmarlsondy/beeradviser/BeerExpert.java
@@ -0,0 +1,20 @@
+package com.example.kevinmarlsondy.beeradviser;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kevin Marlson Dy on 9/27/2015.
+ */
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+        if (color.equals("amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
